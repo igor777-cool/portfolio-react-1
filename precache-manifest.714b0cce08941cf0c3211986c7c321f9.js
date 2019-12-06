3421711fd2f62447e0418ebadd379496f70ab5c3
igor777-cool <igor_baranov@tut.by>
2019-12-06 17:08:59 +0300

    Updates

---
+++ precache-manifest.714b0cce08941cf0c3211986c7c321f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfd28eeaafd5cc2618176c69219055f",
+    "url": "/portfolio-react-1/index.html"
+  },
+  {
+    "revision": "56413f299ecbf68b0b16",
+    "url": "/portfolio-react-1/static/css/main.4ec49cb2.chunk.css"
+  },
+  {
+    "revision": "3164444f975057137fa3",
+    "url": "/portfolio-react-1/static/js/2.a65c4149.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-react-1/static/js/2.a65c4149.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56413f299ecbf68b0b16",
+    "url": "/portfolio-react-1/static/js/main.6e7876e7.chunk.js"
+  },
+  {
+    "revision": "ff801f2cb0385a8e2115",
+    "url": "/portfolio-react-1/static/js/runtime-main.23fc80f1.js"
+  }
+]);
